3e9b6bc74e1f135f8b9ec2409447f3068f8e5103
UnityDeveloper007 <111509949+UnityDeveloper007@users.noreply.github.com>
2022-08-23 14:27:21 +0200

    InGameMenuAdded
    InGameMenuAdded

---
+++ Assets/C#Scripts/InGameMenuController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+public class InGameMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject InGameMenuParent;
+    [SerializeField]
+    private GameObject InGameMenuOptionsParent;
+    public void ExitGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+    }
+    public void OptionsAction()
+    {
+        InGameMenuParent.SetActive(false);
+        InGameMenuOptionsParent.SetActive(true);
+    }
+    public void ResumeAction()
+    {
+        InGameMenuParent.SetActive(false);
+    }
+    public void CloseOptionsMenuAction()
+    {
+        InGameMenuOptionsParent.SetActive(false);
+        InGameMenuParent.SetActive(true);
+    }
+    public void OpenGameMenu()
+    {
+        InGameMenuParent.SetActive(true);
+    }
+}
