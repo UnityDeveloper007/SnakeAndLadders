4e741be6d4fdfdf45efb11e34125246be5f9b67a
UnityDeveloper007 <111509949+UnityDeveloper007@users.noreply.github.com>
2022-08-24 15:25:33 +0200

    MainMenuAdded
    MainMenuAdded

---
+++ Assets/C#Scripts/Menus Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MainMenuParent;
+    [SerializeField]
+    private GameObject MainMenuOptionsParent;
+    [SerializeField]
+    private Scenes StartSceneindex;
+    public void ExitGameAction()
+    {
+        Application.Quit();
+    }
+    public void OptionsAction()
+    {
+        MainMenuParent.SetActive(false);
+        MainMenuOptionsParent.SetActive(true);
+    }
+    public void NewGameAction()
+    {
+        string SceneName = AddSpace(StartSceneindex.ToString());
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+    }
+    private string AddSpace(string text)
+    {
+        string Word = text;
+        if (Word.Contains("_"))
+        {
+            return Word.Replace("_", " ");
+        }
+        return Word;
+    }
+    public void LoadGameAction()
+    {
+        //still need to confrim on saving method and all related Features and Functionalities
+    }
+    public void OptionsBackAction()
+    {
+        MainMenuOptionsParent.SetActive(false);
+        MainMenuParent.SetActive(true);
+    }
+}
---
+++ Assets/C#Scripts/SceneManagement/Scenes.cs
@@ -0,0 +1,5 @@
+public enum Scenes
+{
+        Level_One,
+        Level_Two,
+}
