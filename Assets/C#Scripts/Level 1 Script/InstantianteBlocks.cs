e4a13d529c5917af47e1f099e1a57a02fc40b577
UnityDeveloper007 <111509949+UnityDeveloper007@users.noreply.github.com>
2022-08-26 17:00:58 +0200

    fiuxes

--- Assets/C#Scripts/Level 1 Script/InstantianteBlocks.cs
+++ Assets/C#Scripts/Level 1 Script/InstantianteBlocks.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 public class InstantianteBlocks : MonoBehaviour
@@ -18,223 +17,191 @@ public class InstantianteBlocks : MonoBehaviour
     // Start is called before the first frame update     m
     void Start()
     {
-        if (FirstColor.Equals(null))
+       if (FirstColor.Equals(null))
             FirstColor = new Color32(255, 255, 225, 100);
         if (FirstColor.Equals(null))
             SecondColor = new Color32(0, 0, 0, 100);
         intantiateBlock(NumberOfBlocks);
     }
-
-
     private void intantiateBlock(int counter)
     {
-        int count = 0;
-      for(int x=0; x <= counter + 1; x++)
-      {  
-           count++;
-           
-         
-           if(x == 10)
-           {
-               Debug.Log(x);
-                for(int y=1; y <= 10; y++){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
-                        }
-                             block.name = y.ToString();
-                 }
+          int count = 0;
+          for(int x=0; x <= counter + 1; x++)
+          {  
+               count++;
+               if(x == 10)
+               {
+                   Debug.Log(x);
+                    for(int y=1; y <= 10; y++){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                          if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                 block.name = y.ToString();
+                     }
 
-           }
+               }
+               if(x == 20)
+               {
+                 // int i = totaltime; i >= 0; i--
+                    for(int y=20; y > 10; y--){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                          if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                 block.name = y.ToString();
+                     }
 
-           if(x == 20)
-           {
-             // int i = totaltime; i >= 0; i--
-                for(int y=20; y > 10; y--){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
-                        }
-                             block.name = y.ToString();
-                 }
+               }
+               if(x == 30)
+               {
+                   Debug.Log(x);
+                     for(int y=21; y <= 30; y++){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                          if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                 block.name = y.ToString();
+                     }
 
-           }
-           
-
-
-          if(x == 30)
-           {
-               Debug.Log(x);
-                 for(int y=21; y <= 30; y++){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
-                        }
-                             block.name = y.ToString();
-                 }
-
-           }
-
-
-           if(x == 40)
-           { 
+               }
+               if(x == 40)
+               { 
              
-                     for(int y=40; y > 30; y--){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
-                        }
-                             block.name = y.ToString();
-                 }
-           }
+                         for(int y=40; y > 30; y--){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                          if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                 block.name = y.ToString();
+                     }
+               }
+               if(x == 50)
+               {
+                     for(int y=41; y <= 50; y++){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                          if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                 block.name = y.ToString();
+                     }
 
-            if(x == 50)
-           {
-                 for(int y=41; y <= 50; y++){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
-                        }
-                             block.name = y.ToString();
-                 }
-
-           }
-
-
-
-
-
-          if(x == 60)
-           { 
+               }
+               if(x == 60)
+               { 
              
-                 for(int y=60; y > 50; y--){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
+                     for(int y=60; y > 50; y--){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                          if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                 block.name = y.ToString();
+                     }
+               }
+               if(x == 70)
+               {
+                    for(int y=61; y <= 70; y++){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                          if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                 block.name = y.ToString();
+                     }
+
+               }
+               if(x == 80)
+               {
+                    for(int y=80; y > 70; y--){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                          if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                 block.name = y.ToString();
+                     }
+                }
+                if(x == 90)
+                {
+                    for(int y=81; y <= 90; y++){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                            if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                    block.name = y.ToString();
                         }
-                             block.name = y.ToString();
-                 }
-           }
 
-
-
-           if(x == 70)
-           {
-                for(int y=61; y <= 70; y++){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
-                        }
-                             block.name = y.ToString();
-                 }
-
-           }
-
-            if(x == 80)
-           {
-                for(int y=80; y > 70; y--){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
-                        }
-                             block.name = y.ToString();
-                 }
-            }
-
-            if(x == 90)
-           {
-               for(int y=81; y <= 90; y++){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
-                        }
-                             block.name = y.ToString();
-                 }
-
-           }
-
-           if(x == 100)
-           {
-                for(int y=100; y > 90; y--){
-                    var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
-                    var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
-                    block.transform.parent = parent.transform;
-                    txtBlock.transform.parent = block.transform;
-                    txtBlock.text = y.ToString();
-                      if(y%2 == 0){
-                        block.GetComponent<Image>().color = FirstColor;
-                        }else{
-                        block.GetComponent<Image>().color = SecondColor;
-                        }
-                             block.name = y.ToString();
-                 }
-            }
-
-            
-
-
-         
-         //  blockTex.text = count.ToString();
-          
-          
-      }
-
+                }
+                if(x == 100)
+                {
+                    for(int y=100; y > 90; y--){
+                        var block = Instantiate(Block, new Vector3(0, 0, 0), Quaternion.identity);
+                        var txtBlock =  Instantiate(blockText, new Vector3(0, 0, 0), Quaternion.identity);
+                        block.transform.parent = parent.transform;
+                        txtBlock.transform.parent = block.transform;
+                        txtBlock.text = y.ToString();
+                          if(y%2 == 0){
+                            block.GetComponent<Image>().color = FirstColor;
+                            }else{
+                            block.GetComponent<Image>().color = SecondColor;
+                            }
+                                 block.name = y.ToString();
+                     }
+                }
+          }
     }
-
     // Update is called once per frame
     void Update()
     {
