c01f4af1efa458248e5e6c34b8d9ef04f129b5e2
UnityDeveloper007 <111509949+UnityDeveloper007@users.noreply.github.com>
2022-09-01 15:00:24 +0200

    Full Game Menu Added
    Full Game Menu Added

--- Assets/C#Scripts/Menus Scripts/MainMenu.cs
+++ Assets/C#Scripts/Menus Scripts/MainMenu.cs
@@ -1,7 +1,10 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using System.Collections.Generic;
 public class MainMenu : MonoBehaviour
 {
+    private Resolution[] resolutions;
     [SerializeField]
     private GameObject MainMenuParent;
     [SerializeField]
@@ -10,25 +13,95 @@ public class MainMenu : MonoBehaviour
     private Scenes StartSceneindex;
     [SerializeField]
     private AudioMixer MainAudioMixer;
-    public void Volume(float Volume)
+    [SerializeField]
+    private TMP_Dropdown ScreenResolutionsDropdown;
+    private void Start()
+    {
+        int CurrentScreenIndex;
+        resolutions = Screen.resolutions;
+        ScreenResolutionsDropdown.ClearOptions();
+        List<string> screenResolutionsList = ConvertArrayToListResolutions(resolutions, out CurrentScreenIndex);
+        ScreenResolutionsDropdown.AddOptions(screenResolutionsList);
+        ScreenResolutionsDropdown.value = CurrentScreenIndex;
+        ScreenResolutionsDropdown.RefreshShownValue();
+    }
+    /// <summary>
+    /// this selects the Correct Resolution
+    /// </summary>
+    /// <param name="ScreenResIndex">
+    /// the actual Chosen Resolution</param>
+    public void SetResolution(int ScreenResIndex)
+    {
+        Resolution resolution = resolutions[ScreenResIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    /// <summary>
+    /// this method changes 
+    /// the screen mode
+    /// </summary>
+    /// <param name="isFullScreen">
+    /// a boolean if the screen is 
+    /// in fullscreen</param>
+    public void SetScreenMode(bool isFullScreen) { Screen.fullScreen = isFullScreen; }
+    /// <summary>
+    /// this method sets 
+    /// the grahpics quality 
+    /// </summary>
+    /// <param name="qualityIndex">
+    /// the index of the quality</param>
+    public void SetGraphicsQuality(int qualityIndex) { QualitySettings.SetQualityLevel(qualityIndex); }
+    /// <summary>
+    /// set the volume 
+    /// across the game
+    /// </summary>
+    /// <param name="Volume">
+    /// the volume level to set</param>
+    public void SetVolume(float Volume)
     {
         MainAudioMixer.SetFloat("GameVolume", Volume);
         //Debug.Log($"this is the current Volume level: {Volume}");
     }
-    public void ExitGameAction()
-    {
-        Application.Quit();
-    }
+    /// <summary>
+    /// quits the game
+    /// </summary>
+    public void ExitGameAction() { Application.Quit(); }
+    /// <summary>
+    /// opens options menu
+    /// </summary>
     public void OptionsAction()
     {
         MainMenuParent.SetActive(false);
         MainMenuOptionsParent.SetActive(true);
     }
+    /// <summary>
+    /// goes to the first level
+    /// </summary>
     public void NewGameAction()
     {
         string SceneName = AddSpace(StartSceneindex.ToString());
         UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
     }
+    /// <summary>
+    /// load the user progress
+    /// </summary>
+    public void LoadGameAction()
+    {
+        //still need to confrim on saving method and all related Features and Functionalities
+    }
+    /// <summary>
+    ///  returns to main menu
+    /// </summary>
+    public void OptionsBackAction()
+    {
+        MainMenuOptionsParent.SetActive(false);
+        MainMenuParent.SetActive(true);
+    }
+    /// <summary>
+    /// it removes the _ in the enum and return it 
+    /// as a string with space
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns>the string with a space</returns>
     private string AddSpace(string text)
     {
         string Word = text;
@@ -38,13 +111,25 @@ public class MainMenu : MonoBehaviour
         }
         return Word;
     }
-    public void LoadGameAction()
+    /// <summary>
+    /// changes the defualt Unity screen Resolutions array 
+    /// to a list and grabs the current Screen Index
+    /// </summary>
+    /// <param name="ScreenResolutions">the screen resolution</param>
+    /// <returns>a list of all available Screen Resolutions</returns>
+    private List<string> ConvertArrayToListResolutions(Resolution[] ScreenResolutions, out int CurrentScreenResolution)
     {
-        //still need to confrim on saving method and all related Features and Functionalities
-    }
-    public void OptionsBackAction()
-    {
-        MainMenuOptionsParent.SetActive(false);
-        MainMenuParent.SetActive(true);
+        CurrentScreenResolution = 0;
+        List<string> ResolutionsList = new List<string>();
+        for (int i = 0; i < ScreenResolutions.Length; i++)
+        {
+            ResolutionsList.Add($"{ScreenResolutions[i].width} X {ScreenResolutions[i].height}");
+            if(ScreenResolutions[i].width == Screen.currentResolution.width 
+                && ScreenResolutions[i].height == Screen.currentResolution.height)
+            {
+                CurrentScreenResolution = i;
+            }
+        }
+        return ResolutionsList;
     }
 }
